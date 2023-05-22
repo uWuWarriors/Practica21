229d3a5daaf81e715cdebc2bc776d4cbb4c7fb85
uWuWarriors <130533851+uWuWarriors@users.noreply.github.com>
2023-05-22 16:42:00 +0600

    Add files via upload

---
+++ 123sd/app/src/main/java/com/example/myapplication/shop_class.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class shop_class extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.store);
+    }
+}
